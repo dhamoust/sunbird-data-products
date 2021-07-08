bbc9ec45893783f90f00aacddc5122ad8aed4ea9
dhamoust <40801485+dhamoust@users.noreply.github.com>
2021-07-08 09:39:22 +0530

    Update CourseEnrollmentModel.scala

--- data-products/src/main/scala/org/sunbird/analytics/model/report/CourseEnrollmentModel.scala
+++ data-products/src/main/scala/org/sunbird/analytics/model/report/CourseEnrollmentModel.scala
@@ -36,7 +36,8 @@ object CourseEnrollmentModel extends BaseCourseMetrics[Empty, BaseCourseMetricsO
     if (data.count() > 0) {
       val configMap = config("reportConfig").asInstanceOf[Map[String, AnyRef]]
       val reportConfig = JSONUtils.deserialize[ReportConfig](JSONUtils.serialize(configMap))
-
+      for ((k,v) <- configMap) printf("key: %s, value: %s\n", k, v)
+      for ((k,v) <- reportConfig) printf("key: %s, value: %s\n", k, v)
       import sqlContext.implicits._
       reportConfig.output.map { f =>
           val df = data.toDF().na.fill(0L)
