68d83f3e23aa705dcf8a465af2ab8e6f5f7154a6
dhamoust <40801485+dhamoust@users.noreply.github.com>
2021-07-10 15:14:06 +0530

    Update CourseEnrollmentJobSRL.scala

--- data-products/src/main/scala/org/sunbird/analytics/job/report/CourseEnrollmentJobSRL.scala
+++ data-products/src/main/scala/org/sunbird/analytics/job/report/CourseEnrollmentJobSRL.scala
@@ -6,12 +6,12 @@ import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobDriver}
 import org.sunbird.analytics.model.report.CourseEnrollmentModel
 
 object CourseEnrollmentJobSRL extends optional.Application with IJob {
-  implicit val className = "org.ekstep.analytics.job.CourseEnrollmentJob"
+  implicit val className = "org.ekstep.analytics.job.CourseEnrollmentJobSRL"
 
   def main(config: String)(implicit sc: Option[SparkContext] = None, fc: Option[FrameworkContext] = None) {
     implicit val sparkContext: SparkContext = sc.getOrElse(null);
     JobLogger.log("Started executing Job")
-    JobDriver.run("batch", config, CourseEnrollmentModel)
+    JobDriver.run("batch", config, CourseEnrollmentSRLModel)
     JobLogger.log("Job Completed.")
   }
 }
